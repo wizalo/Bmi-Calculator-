69c2dc0ccb27100780a74ed8f2b5414ce5f09947
wizalo <wichchakrrunlxy@gmail.com>
2026-04-08 23:22:17 +0700

    first upload BMI project

---
+++ bmi_calculator.py
@@ -0,0 +1,13 @@
+
+
+w = float(input("weight(kg) :  "))
+h = float(input("Height(cm) :  "))
+
+bmi = w/(h/100)**2
+
+
+print ("Weight",int(w),"kg")
+print ("Height",int(h),"cm")
+print ("BMI",bmi)
+
+
