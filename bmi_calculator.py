14d83b3636080bdd7a1ef8a2cc292e7569185cca
wizalo <wichchakrrunlxy@gmail.com>
2026-04-09 15:01:51 +0700

    Clear up code and adjust logic

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -8,17 +8,17 @@ bmi = w/(h/100)**2
 
 print ("Weight",int(w),"kg")
 print ("Height",int(h),"cm")
-print ("BMI",bmi)
+print ("BMI",(f"{bmi:.2f}"))
 
 if not (bmi >= 18.5):
     print("You are Underweight")
-elif  18.5 < bmi <= 24.9:
+elif  bmi < 25.0 :
     print("You are Standard weight")
-elif 25 <= bmi <= 29.9:
+elif bmi < 30.0 :
     print("You are Overweight")     
-elif 30 <= bmi <= 34.9:
+elif bmi < 35.0 :
     print("You are Obese class I")
-elif 35 <= bmi <= 39.9:
+elif bmi < 40.0 :
     print("You are Obese class II")     
-elif bmi >= 40:
+elif bmi >= 40 :
     print("You are Obese class III")
