6d7a955abfb9f871d761f87454c15d1caba7759d
wizalo <wichchakrrunlxy@gmail.com>
2026-04-09 12:08:51 +0700

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -10,4 +10,15 @@ print ("Weight",int(w),"kg")
 print ("Height",int(h),"cm")
 print ("BMI",bmi)
 
-
+if not (bmi >= 18.5):
+    print ("You are Underweight")
+if bmi >= 18.6 and bmi <= 24.9:
+    print ("You are Normal weight")
+if bmi >= 25.0 and bmi < 29.9:
+    print ("You are overweight")  
+if bmi >= 30.0 and bmi < 34.9:
+    print ("You are Obesity class I") 
+if bmi >= 35.0 and bmi < 39.9:
+    print ("You are Obesity class II")  
+if bmi >= 40.0:
+    print ("You are Obesity class III")
